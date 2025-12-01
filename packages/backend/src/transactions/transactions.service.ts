04c1e288e634be940599d94220cb5d781c7e8dba
aditya172926 <aditya26sg@gmail.com>
2025-12-01 21:21:44 +0530

    feat: add logic to fetch transaction receipt

--- packages/backend/src/transactions/transactions.service.ts
+++ packages/backend/src/transactions/transactions.service.ts
@@ -66,4 +66,30 @@ export class TransactionsService {
             );
         }
     }
+    
+    async fetch_transaction_data(transaction_hash: string) {
+        const alchemy_api = `${process.env.ALCHEMY_BASE_URL}/${process.env.ALCHEMY_API_KEY}`;
+
+        const transaction_receipt_payload = {
+            id: 1,
+            jsonrpc: "2.0",
+            method: "eth_getTransactionReceipt",
+            params: [transaction_hash]
+        };
+
+        try {
+            const response = this.httpService.post(alchemy_api, transaction_receipt_payload);
+            let {data} = await firstValueFrom(response);
+            return data;
+        } catch (error) {
+            throw new HttpException(
+                {
+                    message: "Failed to fetch transaction receipt for transaction hash",
+                    error: error?.message ?? error.toString(),
+                    transaction_hash
+                },
+                HttpStatus.BAD_GATEWAY
+            );
+        }
+    }
 }
