feeac37e8a3fe01b06e55917e94ec212639156ff
aditya172926 <aditya26sg@gmail.com>
2025-12-01 21:30:06 +0530

    add transaction hash validation pipe

---
+++ packages/backend/src/pipes/transaction_hash/transaction_hash.pipe.ts
@@ -0,0 +1,20 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class TransactionHashPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (typeof value !== 'string') {
+      throw new BadRequestException('Transaction hash must be a string');
+    }
+
+    const trimmed = value.trim();
+
+    const TX_HASH_REGEX = /^0x[a-fA-F0-9]{64}$/;
+
+    if (!TX_HASH_REGEX.test(trimmed)) {
+      throw new BadRequestException(`Invalid Transaction hash ${value}`);
+    }
+
+    return trimmed.toLowerCase();
+  }
+}
