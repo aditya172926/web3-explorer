a856ca6283cea7197684b199ef926b95382e62c4
aditya172926 <aditya26sg@gmail.com>
2025-12-22 19:04:13 +0530

    fix build

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import {RouterProvider} from 'react-router-dom';
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
 
 import {
   getDefaultConfig,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { WagmiProvider } from 'wagmi';
 import {
   mainnet
 } from 'wagmi/chains';
-import {
-  QueryClientProvider,
-  QueryClient,
-} from "@tanstack/react-query";
 import { router } from './routes.ts';
 
 const config = getDefaultConfig({
