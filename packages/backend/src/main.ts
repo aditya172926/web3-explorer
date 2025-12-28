6ea5258e7a85c232d9ae8fc5a2af8e79974dfab0
aditya172926 <aditya26sg@gmail.com>
2025-12-28 11:12:09 +0530

    modify cors list

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Enable CORS for frontend communication
   app.enableCors({
-    origin: ["https://safe-code-challenge-fullstack-front.vercel.app", process.env.FRONTEND_URL, "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
